ddd99d0379cde669e4a79c2ad1b34515627c570a
Mano <62976012+ANRI-0710@users.noreply.github.com>
2021-06-03 12:52:40 +0900

    Update 03.Novel_99_Modules.cpp
    空白行を削除

--- program/game/03.Novel_99_Modules.cpp
+++ program/game/03.Novel_99_Modules.cpp
@@ -9,24 +9,22 @@
 #include "03.Novel_4.h"
 #include "03.Novel_99_Modules.h"
 
-//yŠÖ”z‰ï˜bƒ{ƒbƒNƒX‚É“ü‚é‚æ‚¤‚ÉƒZƒŠƒt‚ğ“ü‚ê‚éŠÖ”EƒZƒŠƒt‚ğŠÖ”‚Å“n‚µ‚Ä•\¦
+//ã€é–¢æ•°ã€‘ä¼šè©±ãƒœãƒƒã‚¯ã‚¹ã«å…¥ã‚‹ã‚ˆã†ã«ã‚»ãƒªãƒ•ã‚’å…¥ã‚Œã‚‹é–¢æ•°ãƒ»ã‚»ãƒªãƒ•ã‚’é–¢æ•°ã§æ¸¡ã—ã¦è¡¨ç¤º
 void::NovelModules::Novel(std::string a, std::string b, std::string c, std::string d) {
 
-	std::string ss1 = a;	//–¼‘O
-	std::string sss[] = { b,c,d };	//ˆês30s‚Ü‚Å	
+	std::string ss1 = a;	//åå‰
+	std::string sss[] = { b,c,d };	//ä¸€è¡Œ30è¡Œã¾ã§	
 	DrawStringEx(230, 475, -1, "%s", ss1.c_str());
 
 	for (int i = 0; i < 3; i++) {
-
 		DrawStringEx(200, 540 + (i * 30), -1, "%s", sss[i].c_str());
 	}
 
 }
 
 void::NovelModules::Novel_Init(NovelModules* novel) {
-
 	
-	//—§‚¿ŠG
+	//ç«‹ã¡çµµ
 	novel->Tatie_00_H = LoadGraph("graphics/Tatie/Tatie_00_H-min.png");
 	novel->Tatie_00_S = LoadGraph("graphics/Tatie/Tatie_00_S-min.png");
 	novel->Tatie_00_I_1 = LoadGraph("graphics/Tatie/Tatie_00_I_1-min.png");
@@ -57,17 +55,12 @@ void::NovelModules::Novel_Init(NovelModules* novel) {
 	novel->Tatie_04_H_2 = LoadGraph("graphics/Tatie/Tatie_04_H_2-min.png");
 	novel->Tatie_04_K = LoadGraph("graphics/Tatie/Tatie_04_K-min.png");
 
-
 	novel->Tatie_05 = LoadGraph("graphics/Tatie/Tatie_07_K-min.png");
 
 	novel->Tatie_MOBU_H = LoadGraph("graphics/Tatie/Tatie_MOBU_H-min.png");
-
-
-
 	
-	//”wŒi
+	//èƒŒæ™¯
 	novel->Back_01 = LoadGraph("graphics/bg_haikei_gakkou.png");
-
 	
 	//BGM
 	novel->Tatie_00_H = LoadGraph("graphics/Tatie/Tatie_00_H-min.png");
@@ -76,22 +69,17 @@ void::NovelModules::Novel_Init(NovelModules* novel) {
 	novel->Tatie_00_I_2 = LoadGraph("graphics/Tatie/Tatie_00_I_2-min.png");
 	novel->Tatie_00_A = LoadGraph("graphics/Tatie/Tatie_00_A-min.png");
 
-
 	novel->Tatie_01_H = LoadGraph("graphics/Tatie/Tatie_01_H-min.png");
 	novel->Tatie_01_I = LoadGraph("graphics/Tatie/Tatie_01_I-min.png");
 	novel->Tatie_01_K = LoadGraph("graphics/Tatie/Tatie_01_K-min.png");
 	novel->Tatie_01_S = LoadGraph("graphics/Tatie/Tatie_01_S-min.png");
-
 
 	novel->Tatie_02_H = LoadGraph("graphics/Tatie/Tatie_02_H-min.png");
 	novel->Tatie_02_I = LoadGraph("graphics/Tatie/Tatie_02_I-min.png");
 	novel->Tatie_02_K = LoadGraph("graphics/Tatie/Tatie_02_K-min.png");
 	novel->Tatie_02_S = LoadGraph("graphics/Tatie/Tatie_02_S-min.png");
 
-
 	novel->Tatie_03_H_kamen = LoadGraph("graphics/Tatie/Tatie_03_H_kamen-min.png");
-
-
 	novel->Tatie_03_H = LoadGraph("graphics/Tatie/Tatie_03_H-min.png");
 	novel->Tatie_03_I = LoadGraph("graphics/Tatie/Tatie_03_I_kamen-min.png");
 	novel->Tatie_03_O = LoadGraph("graphics/Tatie/Tatie_03_O-min.png");
@@ -101,31 +89,24 @@ void::NovelModules::Novel_Init(NovelModules* novel) {
 	novel->Tatie_04_H = LoadGraph("graphics/Tatie/Tatie_04_H-min.png");
 	novel->Tatie_04_H_2 = LoadGraph("graphics/Tatie/Tatie_04_H_2-min.png");
 	novel->Tatie_04_K = LoadGraph("graphics/Tatie/Tatie_04_K-min.png");
-
 	novel->Tatie_04_02_H = LoadGraph("graphics/Tatie/Tatie_04_02_H-min.png");
 	novel->Tatie_04_02_H_2 = LoadGraph("graphics/Tatie/Tatie_04_02_H_2-min.png");
 	novel->Tatie_04_02_K = LoadGraph("graphics/Tatie/Tatie_04_02_K-min.png");
 
-
 	novel->Tatie_05 = LoadGraph("graphics/Tatie/Tatie_07_K-min.png");
-
 	novel->Tatie_MOBU_H = LoadGraph("graphics/Tatie/Tatie_MOBU_H-min.png");
-
 
 	novel->Back_01 = LoadGraph("graphics/Background/bg_haikei_gakkou-min.png");
 	novel->Back_02 = LoadGraph("graphics/Background/bg_haikei_tika-min.png");
 	novel->Back_03 = LoadGraph("graphics/Background/bg_haikei_kyousitu-min.png");
 	novel->Back_04 = LoadGraph("graphics/Background/bg_haikei_rouka-min.png");
 	novel->Back_05 = LoadGraph("graphics/Background/bg_haikei_sakura-min.png");
-
-
+	
 	novel->BGM1 = LoadSoundMem("sound/BGM1.mp3");
 	novel->BGM2 = LoadSoundMem("sound/BGM2.mp3");
 	novel->BGM3 = LoadSoundMem("sound/BGM3.mp3");
 	novel->BGM4 = LoadSoundMem("sound/BGM4.mp3");
 	novel->BGM5 = LoadSoundMem("sound/BGM5.mp3");
-
-
 }
 
 
@@ -182,45 +163,24 @@ void::NovelModules::Novel_Fin(NovelModules* novel) {
 	DeleteSoundMem(novel->BGM3);
 	DeleteSoundMem(novel->BGM4);
 	DeleteSoundMem(novel->BGM5);
-
-
-
-
-
-
 }
 
 
 
 void::NovelModules::Tatie_LEFT(int tatie) {
-
-
 	DrawRotaGraph(200, 450, 1.0f, 0, tatie, true);
-
-
 }
 
 void::NovelModules::Tatie_CENTER(int tatie) {
 
-
-
-	DrawRotaGraph(500, 450, 1.0f, 0, tatie, true);
-
-
+ã€€ã€€ã€€ã€€DrawRotaGraph(500, 450, 1.0f, 0, tatie, true);
 }
 
 void::NovelModules::Tatie_Right(int tatie) {
 
-
 	DrawRotaGraph(800, 450, 1.0f, 0, tatie, true);
-
-
 }
 
 void::NovelModules::Tatie_BACKIMAGE(int tatie) {
-
-
 	DrawRotaGraph(600, 150, 1.0f, 0, tatie, true);
-
-
 }
