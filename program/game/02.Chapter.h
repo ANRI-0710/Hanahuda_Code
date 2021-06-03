c9fd6c48084bf06da77e188584a152bea76b2f59
Mano <62976012+ANRI-0710@users.noreply.github.com>
2021-06-03 12:53:31 +0900

    Update 02.Chapter.h
    空白行を削除

--- program/game/02.Chapter.h
+++ program/game/02.Chapter.h
@@ -1,56 +1,42 @@
 #pragma once
 
 //---------------------------------------------
-//�`���v�^�[�V�[���̊Ǘ��N���X
+//チャプターシーンの管理クラス
 //---------------------------------------------
 
-//�`���v�^�[�A�����b�N�̐���N���X
+//チャプターアンロックの制御クラス
 class Chapter_Key {
 public:
-
-	bool Chapter_Unlock[5] = { 1,0,0,0,0 };	//�`���v�^�[����p
-
+	bool Chapter_Unlock[5] = { 1,0,0,0,0 };	//チャプター制御用
 };
 
 enum eChapter_Unlock {
-
-	eUnlock_Chpter_0,	//�`���v�^�[0�b�E�v�����[�O�̂��ߍŏ�������
-	eUnlock_Chpter_1,	//�`���v�^�[1�b�A0�b�Ǘ����������
-	eUnlock_Chpter_2,	//�`���v�^�[2�b�A1�b�ł̉ԎD�̐퓬�ŏ������������
-	eUnlock_Chpter_3,	//�`���v�^�[3�b�A2�b�ł̉ԎD�̐퓬�ŏ������������
-	eUnlock_Chpter_4,	//�`���v�^�[�ŏI�b�A3�b�ł̉ԎD�̐퓬�ŏ������������
-
+	eUnlock_Chpter_0,	//チャプター0話・プロローグのため最初から解放
+	eUnlock_Chpter_1,	//チャプター1話、0話読了後解放される
+	eUnlock_Chpter_2,	//チャプター2話、1話での花札の戦闘で勝利後解放される
+	eUnlock_Chpter_3,	//チャプター3話、2話での花札の戦闘で勝利後解放される
+	eUnlock_Chpter_4,	//チャプター最終話、3話での花札の戦闘で勝利後解放される
 };
 
 
-//�Z�[�u�f�[�^�Ǘ�
+//セーブデータ管理
 struct save_deta_t {
-
 	bool s_Chapter_Unlock[5] = {1,0,0,0,0};
-
-
 };
 
-
-
-
-
-
-//������
+//初期化
 void Chapter_Initialize();
 
-//�I������
+//終了処理
 void Chapter_Finalize();
 
-
-//�X�V
+//更新
 void Chapter_Update();
 
-	//�X�V-�`���v�^�[��������p�֐�
+	//更新-チャプター制御解除用関数
 	void Chapter_Unlock(int *chapter_NUM);
 
-
-//�`��
+//描画
 void Chapter_Draw();
 
 
